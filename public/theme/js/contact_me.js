ab890a29e57aa88209ef32d033d4748a914a0f78
Katrina Rodriguez <karodriguez8@gmail.com>
2016-06-27 16:16:05 -0400

    typos in contact form

--- public/theme/js/contact_me.js
+++ public/theme/js/contact_me.js
@@ -12,14 +12,14 @@ $(function() {
             var email = $("input#email").val();
             var phone = $("input#phone").val();
             var message = $("textarea#message").val();
-            // var firstName = name; // For Success/Failure Message
+            var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
-            // if (firstName.indexOf(' ') >= 0) {
-            //     firstName = name.split(' ').slice(0, -1).join(' ');
-            // }
+            if (firstName.indexOf(' ') >= 0) {
+                firstName = name.split(' ').slice(0, -1).join(' ');
+            }
             $.ajax({
-                url: "api/inquiry",
-                type: "post",
+                url: "././mail/contact_me.php",
+                type: "POST",
                 data: {
                     name: name,
                     phone: phone,
@@ -45,7 +45,7 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry, it seems that the mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
